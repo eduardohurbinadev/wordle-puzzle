32aed35f4725e70a0c370c4d37e15edbf48ecef8
Eduardo Urbina <eduardoh.urbina.developer@gmail.com>
2023-03-20 04:53:14 -0600

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
